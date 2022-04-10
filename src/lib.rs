66262da21f0a90c80a69bd3af580f46e444205a3
Jonathan Bosi <jbosi@lucca.fr>
2022-04-10 21:15:04 +0200

    Fix checksum, rework error and add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,139 @@
 use std::{str::Chars, fmt};
 use wasm_bindgen::prelude::*;
 
-const LETTER_A_VALUE: u16 = 'A' as u16;
-const ASCII_OFFSET_TO_ZERO: u16 = 48;
-const LETTER_OFFSET_TO_CHECKSUM: u16 = 10;
+const LETTER_A_VALUE: u128 = 'A' as u128;
+const ASCII_OFFSET_TO_ZERO: u128 = 48;
+const LETTER_OFFSET_TO_CHECKSUM: u128 = 10;
 
 #[wasm_bindgen]
-pub fn validate(raw_input: &str) -> Result<bool, JsError> {
+pub fn validate(raw_input: &str) -> Result<bool, String> {
 	validate_not_null(raw_input)?;
-
+	
 	let iban_candidate: String = raw_input.chars()
-		.filter(|c| !c.is_whitespace())
-		.collect();
-
+	.filter(|c| !c.is_whitespace())
+	.collect();
+	
 	validate_length(&iban_candidate)?;
 	validate_first_letters(&iban_candidate)?;
 	validate_checksum(&iban_candidate)?;
-
+	
 	return Ok(true);
 }
 
-fn validate_not_null(iban_candidate: &str) -> Result<bool, JsError> {
+fn validate_not_null(iban_candidate: &str) -> Result<bool, String> {
 	if iban_candidate.is_empty() {
-		return Err(JsError::new(&ValidationErrorCodes::E01.to_string()));
+		return Err(ValidationErrorCodes::ErrorNullValue.to_string());
 	}
 	return Ok(true);
 }
 
-fn validate_length(iban_candidate: &str) -> Result<bool, JsError> {
+fn validate_length(iban_candidate: &str) -> Result<bool, String> {
 	if iban_candidate.len() <= 34 {
 		return Ok(true);
 	};
-	return Err(JsError::new(&ValidationErrorCodes::E02.to_string()));
+	return Err(ValidationErrorCodes::ErrorMaxLength.to_string());
 }
 
-fn validate_first_letters(iban_candidate: &str) -> Result<bool, JsError> {
+fn validate_first_letters(iban_candidate: &str) -> Result<bool, String> {
 	let mut chars: Chars = iban_candidate.chars();
 	let first_char: char = chars.next().unwrap();
 	let second_char: char = chars.next().unwrap();
-
+	
 	if first_char.is_ascii_alphabetic()
 		&& first_char.is_uppercase()
 		&& second_char.is_ascii_alphabetic()
 		&& second_char.is_uppercase() {
 		return Ok(true);
 	}
-
-	return Err(JsError::new(&ValidationErrorCodes::E03.to_string()));
+	
+	return Err(ValidationErrorCodes::ErrorFirstTwoLetters.to_string());
 }
 
-fn validate_checksum(iban_candidate: &str) -> Result<bool, JsError> {
+fn validate_checksum(iban_candidate: &str) -> Result<bool, String> {
 	let iban_without_four_first_block: String = iban_candidate[4..iban_candidate.len()].to_string();
 	let first_chars = &iban_candidate[0..4];
-
+	
 	let shuffled_iban = iban_without_four_first_block + first_chars;
-
-	let result: u16 = shuffled_iban.chars().map(|val| {
+	
+	let result: String = shuffled_iban.chars().map(|val| {
+		let number;
 		if val.is_ascii_alphabetic() {
-			return (val as u16) - LETTER_A_VALUE + LETTER_OFFSET_TO_CHECKSUM;
+			number = (val as u128) - LETTER_A_VALUE + LETTER_OFFSET_TO_CHECKSUM;
+		} else {
+			number = (val as u128) - ASCII_OFFSET_TO_ZERO;
 		}
-		return (val as u16) - ASCII_OFFSET_TO_ZERO;
-	}).sum();
-
-	if (result / 97) == 1 {
+		return number.to_string();
+	}).collect();
+	
+	if result.parse::<u128>().unwrap().rem_euclid(97) == 1 {
 		return Ok(true);
 	};
+	
+	return Err(ValidationErrorCodes::ErrorChecksum.to_string());
+}
 
-	return Err(JsError::new(&ValidationErrorCodes::E04.to_string()));
+#[cfg(test)]
+mod tests {
+	use crate::{validate, ValidationErrorCodes};
+	const VALID_IBAN_1: &str = "FR7630006000011234567890189";
+	const VALID_IBAN_2: &str = "DE75 5121 0800 1245 1261 99";
+	const VALID_IBAN_3: &str = "GB33BUKB20201555555555";
+	
+	#[test]
+	fn should_error_for_max_lenth() {
+		let validation = validate("FR7630006000011234567890189 FR7630006000011234567890189 FR7630006000011234567890189");
+		assert_eq!(ValidationErrorCodes::ErrorMaxLength.to_string(), validation.unwrap_err());
+		
+	}
+
+	#[test]
+	fn should_error_if_no_input_value() {
+		let validation = validate("");
+		assert_eq!(ValidationErrorCodes::ErrorNullValue.to_string(), validation.unwrap_err());
+	}
+
+	#[test]
+	fn should_error_if_invalid_checksum() {
+		let validation = validate("FR7630006000011234567890183");
+		assert_eq!(ValidationErrorCodes::ErrorChecksum.to_string(), validation.unwrap_err());
+	}
+
+	#[test]
+	fn should_error_if_invalid_two_first_char() {
+		let validation = validate("F37630006000011234567890189");
+		assert_eq!(ValidationErrorCodes::ErrorFirstTwoLetters.to_string(), validation.unwrap_err());
+	}
+
+	#[test]
+	fn should_return_valid() {
+		let validation_1 = validate(VALID_IBAN_1);
+		let validation_2 = validate(VALID_IBAN_2);
+		let validation_3 = validate(VALID_IBAN_3);
+		assert_eq!(true, validation_1.unwrap());
+		assert_eq!(true, validation_2.unwrap());
+		assert_eq!(true, validation_3.unwrap());
+	}
 }
 
 #[wasm_bindgen]
 pub enum ValidationErrorCodes {
-	E01,
-	E02,
-	E03,
-	E04,
+	ErrorNullValue,
+	ErrorMaxLength,
+	ErrorFirstTwoLetters,
+	ErrorChecksum,
 	E05,
 	E06
 }
 
 impl fmt::Display for ValidationErrorCodes {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ValidationErrorCodes::E01 => write!(f, "E01"),
-            ValidationErrorCodes::E02 => write!(f, "E02"),
-            ValidationErrorCodes::E03 => write!(f, "E03"),
-            ValidationErrorCodes::E04 => write!(f, "E04"),
-            ValidationErrorCodes::E05 => write!(f, "E05"),
-            ValidationErrorCodes::E06 => write!(f, "E06"),
-        }
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			ValidationErrorCodes::ErrorNullValue => write!(f, "ErrorNullValue"),
+			ValidationErrorCodes::ErrorMaxLength => write!(f, "ErrorMaxLength"),
+			ValidationErrorCodes::ErrorFirstTwoLetters => write!(f, "ErrorFirstTwoLetters"),
+			ValidationErrorCodes::ErrorChecksum => write!(f, "ErrorChecksum"),
+			ValidationErrorCodes::E05 => write!(f, "E05"),
+			ValidationErrorCodes::E06 => write!(f, "E06"),
+		}
+	}
 }
